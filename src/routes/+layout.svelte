69bc1430c42e30df9043f11836aacde8f6a44e9b
JasperKelder <jasperkelder@gmail.com>
2023-03-01 19:52:22 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '@picocss/pico';
+	import '../styles.scss';
+</script>
+
+<svelte:head>
+	<title>SvelteKit AI - ELI5</title>
+</svelte:head>
+
+<main class="container">
+	<slot />
+</main>
